8a7d970c34f319d393fb5ebd267dd9a4bcf800b4
takuma-tsx <i.am.geek@techroid.jp>
2025-07-23 11:59:22 +0000

    firebase

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,5 +18,5 @@ export default function LoginPage() {
         </button>
       </form>
     </div>
-  )
+  );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -23,5 +23,5 @@ export default function SignupPage() {
         </button>
       </form>
     </div>
-  )
+  );
 }
